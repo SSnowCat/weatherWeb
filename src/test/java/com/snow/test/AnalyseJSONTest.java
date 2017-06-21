2dd531061fdfe74d49232572ce7815a07d9331d4
Virtuesun <softsmile@126.com>
2017-06-21 09:29:48 +0800

    pull新代码

---
+++ src/test/java/com/snow/test/AnalyseJSONTest.java
@@ -0,0 +1,16 @@
+package com.snow.test;
+
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/6/20.
+ */
+public class AnalyseJSONTest {
+    private static final String LAT = "39.91488908";
+    private static final String LON = "116.40387397";
+
+    @Test
+    public void testGetCity(){
+
+    }
+}
