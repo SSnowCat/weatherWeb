f7c201ec8fad52a81c8a77dcf31331fe2321c22d
Virtuesun <softsmile@126.com>
2017-06-21 17:31:54 +0800

    修改静态页为jsp页面 定义页面api 绘图js完成（缺数据）

--- src/main/java/com/snow/weather/controller/MainController.java
+++ src/main/java/com/snow/weather/controller/MainController.java
@@ -2,10 +2,13 @@ package com.snow.weather.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpSession;
+
 /**
- * Created by Administrator on 2017/6/21.
+ * Created by 李德阳 on 2017/6/21.
  */
 @Controller
 public class MainController {
@@ -15,10 +18,22 @@ public class MainController {
         return "index";
     }
 
-    @GetMapping("/getChartData")
+    @GetMapping(value = "/getChartData",produces = "application/json;charset=utf-8")
     @ResponseBody
     public String getChartData(){
         String data = "[{\"Ftemp\":26,\"Fpredict_hour\":12,\"wind_level\":2},{\"Ftemp\":27,\"Fpredict_hour\":13,\"wind_level\":2},{\"Ftemp\":26,\"Fpredict_hour\":14,\"wind_level\":2},{\"Ftemp\":27,\"Fpredict_hour\":15,\"wind_level\":2}]";
         return data;
+    }
+
+    @GetMapping(value = "/citySearch/{word}",produces = "application/json;charset=utf-8")
+    @ResponseBody
+    public String getCitySearch(@PathVariable String word){
+        String data = "{\"city_list\":[{\"cityId\":2635,\"city_lable\":[],\"counname\":\"中国\",\"id\":53,\"localCounname\":\"中国\",\"localName\":\"成都市\",\"localPname\":\"四川省\",\"name\":\"成都市\",\"pname\":\"四川省\"}],\"rc\":{\"c\":0}}";
+        return data;
+    }
+
+    @GetMapping(value = "/weather/{name}")
+    public String getWeather(@PathVariable String name,HttpSession session){
+        return "redirect: /index";
     }
 }
