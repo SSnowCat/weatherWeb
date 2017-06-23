3325704dda3af532666b87e10dcbd38fdb3f3f68
hyg5820 <hyg_5820@163.com>
2017-06-23 10:21:55 +0800

    service完善

--- src/main/java/com/snow/weather/service/UserService.java
+++ src/main/java/com/snow/weather/service/UserService.java
@@ -8,15 +8,24 @@ import  com.snow.weather.domain.City;
 import com.snow.weather.domain.Life;
 import com.snow.weather.domain.Temp;
 import com.snow.weather.domain.Weather;
+import com.snow.weather.vo.WeatherDetails;
 
+import java.lang.management.ThreadMXBean;
+import java.util.Date;
 import java.util.List;
 
 public interface UserService {
 
 
+    WeatherDetails getWeatherDetails(City city);//天气实况
     List<Temp> getTemp(City city);//获取24小时温度
 
     List<Weather> getTwoWeeksWeather(City city);//获取两周天气
 
     List<Life> getLifeIndex(City city);//获取生活指数
+
+    Weather getCurrentWeather(City city);
+
+    Temp getCurrentTemp(City city);
+
 }
